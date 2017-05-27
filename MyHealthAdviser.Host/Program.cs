980ede1af7e34dd56d9a5027c8b5c7074c9c6ec9
mahaaraj <mahaaraj@gmail.com>
2017-05-27 13:01:02 +0530

    Initial

---
+++ MyHealthAdviser.Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+namespace MyHealthAdviser.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase[] servicestoRun = new ServiceBase[]
+            {
+
+               new MyHealthAdviserService()
+            };
+
+            ServiceBase.Run(servicestoRun);
+
+
+        }
+    }
+}
---
+++ MyHealthAdviser.Services/IHealthAdviserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyHealthAdviser.Dto;
+namespace MyHealthAdviser.Contracts
+{
+    public interface IHealthAdviserService
+    {
+        bool Submit(User user);
+        
+    }
+}
